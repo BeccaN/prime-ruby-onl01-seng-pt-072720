22eac1691d36a6c55e2a6bf259d06c4bf96e7b3c
Rebecca Congi <rncongi@gmail.com>
2020-08-05 22:12:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,29 +1,13 @@
-# def prime?(num)
-#   if num < 2
-#     return false
-#   elsif num == 2
-#     return true 
-#   elsif num > 2 
-#     if (2..num).find{|n| num % n == 0}
-#       return false
-#     else
-#       return true 
-#     end
-#   end 
-# end 
-
-def is_prime?(max)
-  i = 2
-  while i < max
-    is_divisible = ((max % i) == 0)
-    if is_divisible
-      # divisor found; stop and return false!
+def prime?(num)
+  if num < 2
+    return false
+  elsif num == 2
+    return true 
+  elsif num > 2 
+    if (2..num).find{|n| num % n == 0}
       return false
+    else
+      return true 
     end
-
-    i += 1
-  end
-
-  # no divisors found
-  true
-end
+  end 
+end 
