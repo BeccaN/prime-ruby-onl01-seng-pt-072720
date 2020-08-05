a1ac733ff6e15bde7ce5cedc98c7de4b65e50cdb
Rebecca Congi <rncongi@gmail.com>
2020-08-05 22:01:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(num)
+  if num < 2
+    return false
+  elsif num == 2
+    return true 
+  end
+  
+  while num > 2 
+    if (2..num).find{|n| num % n == 0}
+      false
+    else
+      true
+    end
+  end 
+end 
