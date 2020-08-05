952657372f4bfe74964665c57e1eab63bbb28991
Rebecca Congi <rncongi@gmail.com>
2020-08-05 22:01:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,11 +5,11 @@ def prime?(num)
     return true 
   end
   
-  while num > 2 
-    if (2..num).find{|n| num % n == 0}
-      false
-    else
-      true
-    end
-  end 
+  # while num > 2 
+  #   if (2..num).find{|n| num % n == 0}
+  #     false
+  #   else
+  #     true
+  #   end
+  # end 
 end 
